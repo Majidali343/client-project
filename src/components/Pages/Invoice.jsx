5658c4490a12227fc2ef69a3781b5e806bff42e9
majidali343 <majidali37406@gmail.com>
2024-08-30 15:06:57 +0500

    some changes in navigation for code betterment

--- src/components/Pages/Invoice.jsx
+++ src/components/Pages/Invoice.jsx
@@ -8,6 +8,7 @@ import html2canvas from "html2canvas";
 import "react-datepicker/dist/react-datepicker.css";
 import axios from 'axios';
 import { saveAs } from 'file-saver';
+import Navigation from "./Navigation";
 
 function Employeetask() {
   const [dropdownOpen, setDropdownOpen] = useState(null);
@@ -147,61 +148,7 @@ function Employeetask() {
             src={Logo}
             alt="Logo"
           />
-          <nav>
-            <Link to="/">
-              <button className="block text-[#3d3d3d] text-sm py-2.5 px-4 rounded hover:bg-[#ea8732] hover:text-white font-bold w-full text-left">
-                Dashboard
-              </button>
-            </Link>
-            <Link to="/Employeesalary">
-              <button className="block text-[#3d3d3d] text-sm py-2.5 px-4 rounded hover:bg-[#ea8732] hover:text-white font-bold w-full text-left">
-                Employee Salary
-              </button>
-            </Link>
-            <Link to="/Employeetask">
-              <button className="block text-[#3d3d3d] text-sm py-2.5 px-4 rounded hover:bg-[#ea8732] hover:text-white font-bold w-full text-left">
-                Employee Task
-              </button>
-            </Link>
-            <Link to="/Income">
-              <button className="block text-[#3d3d3d] text-sm py-2.5 px-4 rounded hover:bg-[#ea8732] hover:text-white font-bold w-full text-left">
-                Income
-              </button>
-            </Link>
-            <Link to="/Expenses">
-              <button className="block text-[#3d3d3d] text-sm py-2.5 px-4 rounded hover:bg-[#ea8732] hover:text-white font-bold w-full text-left">
-                Expenses
-              </button>
-            </Link>
-            <Link to="/Customer">
-              <button className="block text-[#3d3d3d] text-sm py-2.5 px-4 rounded hover:bg-[#ea8732] hover:text-white font-bold w-full text-left">
-                Customers
-              </button>
-            </Link>
-            <Link to="/Vehicle">
-              <button className="block text-[#3d3d3d] text-sm py-2.5 px-4 rounded hover:bg-[#ea8732] hover:text-white font-bold w-full text-left">
-                Vehicles
-              </button>
-            </Link>
-            <Link to="/Pending">
-              <button className="block text-[#3d3d3d] text-sm py-2.5 px-4 rounded hover:bg-[#ea8732] hover:text-white font-bold w-full text-left">
-                Pending
-              </button>
-            </Link>
-            <Link to="/Invoice">
-              <button className="block text-[#3d3d3d] text-sm py-2.5 px-4 rounded hover:bg-[#ea8732] hover:text-white font-bold w-full text-left">
-                Invoice
-              </button>
-            </Link>
-            <Link to="/Report">
-              <button className="block text-[#3d3d3d] text-sm py-2.5 px-4 rounded hover:bg-[#ea8732] hover:text-white font-bold w-full text-left">
-              Invoice Generate
-              </button>
-            </Link>
-            <button className="block text-[#3d3d3d] text-sm py-2.5 px-4 rounded hover:bg-[#ea8732] hover:text-white font-bold w-full text-left">
-              Sign Out
-            </button>
-          </nav>
+         <Navigation/>
         </div>
       </aside>
       <div className="flex-1 flex flex-col ml-64">
